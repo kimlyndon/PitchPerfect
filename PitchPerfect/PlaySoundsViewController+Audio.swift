de80998499c92f48724a7b60407e6548c904082c
Kim Lyndon <kimmylyndon@gmail.com>
2018-04-10 10:05:06 -0400

    first commit

---
+++ PitchPerfect/PlaySoundsViewController+Audio.swift
@@ -0,0 +1,9 @@
+//
+//  PlaySoundsViewController+Audio.swift
+//  PitchPerfect
+//
+//  Created by Kim Lyndon on 3/24/18.
+//  Copyright © 2018 Kim Lyndon. All rights reserved.
+//
+
+import Foundation
---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Kim Lyndon on 3/24/18.
+//  Copyright © 2018 Kim Lyndon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
+    }
+}
+
